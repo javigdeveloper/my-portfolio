a7f8fd8557ddd39005e594d0ee4872d6d5759e99
Javier García <javigdeveloper@gmail.com>
2020-10-04 22:46:10 -0500

    hiding and showing images in mid section

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let mailImg = document.querySelector("#mail");
 let modal = document.querySelector(".modal");
 let body = document.querySelector("body");
+let mid = document.querySelector(".technologies");
+let span = document.querySelector(".span");
 
+// email modal:
 mailImg.addEventListener("click", () => {
   modal.classList.add("show");
 });
@@ -9,5 +12,18 @@ mailImg.addEventListener("click", () => {
 body.addEventListener("click", (e) => {
   if (e.target.classList.contains("about-me")) {
     modal.classList.remove("show");
+  }
+});
+
+// mid section instructions:
+mid.addEventListener("mouseenter", (e) => {
+  if (e.target.classList.contains("technologies")) {
+    span.classList.add("viewing");
+  }
+});
+
+mid.addEventListener("mouseleave", (e) => {
+  if (e.target.classList.contains("technologies")) {
+    span.classList.remove("viewing");
   }
 });
